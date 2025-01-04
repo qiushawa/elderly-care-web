c977085a939f92316572f0328fce13f85820bf12
Qiushawa <qiushawa@gmail.com>
2025-01-05 01:26:45 +0800

    更新 FFmpeg 伺服器邏輯，調整串流路徑及參數以改善串流品質

--- app/module/util/ffmpeg.py
+++ app/module/util/ffmpeg.py
@@ -3,18 +3,21 @@ import subprocess
 
 def server(stream_id, port):
     """使用 FFmpeg 串流"""
-    # ffmpeg -i https://stream.qiushawa.me/hls/... -an -c:v mjpeg -q:v 10 -r 15 -f mjpeg tcp://127.0.0.1:8011
-
+    # ffmpeg -i https://stream.qiushawa.me/hls/qiushawa.m3u8 -f mjpeg -q:v 3 -r 25 tcp://127.0.0.1:8081 
     command = [
         "ffmpeg",
-        "-i", f"http://127.0.0.1:80/hls/{stream_id}.m3u8",
-        "-an",
-        "-c:v", "mjpeg",
-        "-q:v", "10",
-        "-r", "15",
-        "-f", "mjpeg",
+        "-i",
+        f"http://eldercare.qiushawa.me/hls/{stream_id}.m3u8",
+        "-f",
+        "mjpeg",
+        "-q:v",
+        "3",
+        "-r",
+        "25",
         f"tcp://127.0.0.1:{port}"
     ]
+    
+    # ffmpeg -i http://eldercare.qiushawa.me/hls/123456.m3u8 -an -c:v mjpeg -q:v 10 -r 15 -f mjpeg tcp://127.0.0.1:8011
     print(f"FFmpeg command for {stream_id}: {' '.join(command)}")
     try:
         subprocess.run(command, check=True)
